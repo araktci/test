7bfcd93d912e908ae9598de1a7346fec03acfea2
Meisam <Meisam@gmail.com>
2022-08-01 13:58:00 +0430

    com 05 from shmp

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import Home from "./page/dashboard/Home";
 import City from "./page/settings/cities/City";
 import Center from "./page/settings/centers/Center";
 
+//comment from shmp
+
 let routes = [
   { path: "/", element: <Home /> },
   { path: "/settings/cities", element: <City /> },
